4175362ca80887988c892edb8e68938608bea490
Modisaotsile Mokgethi <modisamokgethi@gmail.com>
2019-10-02 14:49:26 +0200

    Handling Null Values

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -4,14 +4,17 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   selector: 'app-event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
+      <h2>{{event?.name}}</h2>
+      <div>Date: {{event?.date}}</div>
+      <div>Time: {{event?.time}}</div>
+      <div>Price: \${{event?.price}}</div>
       <div>
-        <span>Location: {{event.location.address}}</span>
+        <span>Location: {{event?.location?.address}}</span>
         <span>&nbsp;</span>
-        <span>{{event.location.city}}, {{event.location.country}}</span>
+        <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
+      </div>
+      <div>
+        Online URL: {{event?.onlineUrl}}
       </div>
     </div>
   `,
